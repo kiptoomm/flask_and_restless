673fb3a08060a61bcafd227e1edd760d25a8f57a
Kiptoo Magutt <kiptoo.magutt@aljazeera.net>
2018-01-13 16:24:35 +0300

    add unit test suite & case for author model
    - failing due to enum not being handled by sqlite properly:
    IntegrityError: (sqlite3.IntegrityError) CHECK constraint failed: author [SQL: u'INSERT INTO author (first_name, last_name, gender) VALUES (?, ?, ?)'] [parameters: ('first1', 'last1', 'U')]

---
+++ tests/test_models.py
@@ -0,0 +1,35 @@
+import unittest
+from flask_testing import TestCase
+from flask_and_restless.models import Author, Book, Gender
+from flask_and_restless import Base, s, app
+
+class TestBase(TestCase):
+
+    def create_app(self):
+        _app = app
+        return _app
+
+    def setUp(self):
+        Base.metadata.drop_all()
+        Base.metadata.create_all()
+
+        author = Author(first_name='first1', last_name='last1')
+        book = Book(title='title1', author_id=author.id, is_available=True)
+
+        s.add(author)
+        s.add(book)
+        s.commit()
+
+    def tearDown(self):
+        s.remove()
+        Base.metadata.drop_all()
+
+class TestModels(TestBase):
+
+    def test_author(self):
+        author = s.query(Author).get(1)
+        self.assertEqual(author.first_name, 'first1')
+
+
+if __name__ == '__main__':
+    unittest.main()
